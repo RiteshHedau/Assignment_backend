343a57692ec80a9d5f5f8f793b9d1b0f67d39c8f
RiteshHedau <riteshhedau7@gmail.com>
2025-04-12 20:17:28 +0530

    commit

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const courseRoutes = require("./routes/course.route");
 
 const app = express();
 const corsOptions = {
-  origin: process.env.CLIENT_URL, // Replace with your frontend URL
+  origin: "*" || process.env.CLIENT_URL, // Replace with your frontend URL
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  exposedHeaders: ["Content-Length", "Authorization"],
+  //methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  // allowedHeaders: ["Content-Type", "Authorization"],
+  // exposedHeaders: ["Content-Length", "Authorization"],
 };
 
 app.use(cors(corsOptions));
